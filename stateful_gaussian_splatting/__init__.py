c222b3b1b09dab67fcf489828cc75b171ec3ea40
pompoi <fredejuhl@gmail.com>
2025-03-08 11:21:33 +0100

    Add main package __init__.py

---
+++ stateful_gaussian_splatting/__init__.py
@@ -0,0 +1,12 @@
+"""
+Stateful Gaussian Splatting
+===========================
+
+A Python liquid neural network project for stateful Gaussian splatting using multi-view camera images.
+"""
+
+__version__ = "0.1.0"
+
+from .splatter import StatefulGaussianSplatter
+
+__all__ = ["StatefulGaussianSplatter"]
