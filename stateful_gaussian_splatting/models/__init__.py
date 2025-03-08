6fef9945d0aa588e2bbe94489cdc39a29ac08170
pompoi <fredejuhl@gmail.com>
2025-03-08 11:22:19 +0100

    Add models __init__.py

---
+++ stateful_gaussian_splatting/models/__init__.py
@@ -0,0 +1,8 @@
+"""
+Model modules for the stateful Gaussian splatting project.
+"""
+
+from .gaussian_model import GaussianModel
+from .liquid_network import LiquidNetwork
+
+__all__ = ["GaussianModel", "LiquidNetwork"]
